3b191eafc65d98a3af56603d09ae62d124a86e97
jswj <jswj30@naver.com>
2022-03-05 22:55:30 +0900

    1

--- tabbarAnimation/App.js
+++ tabbarAnimation/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+import TestSvg from './assets/img/testsvg.svg';
+
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>123 </Text>
+      <View style={{
+        borderWidth: 2, 
+        width: 100, 
+        height: 100, 
+      }}>
+        <TestSvg 
+          width={'100%'}
+          height={'100%'}
+        />
+      </View>
     </View>
   );
 }
@@ -13,7 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, 
     justifyContent: 'flex-end', 
-    backgroundColor: '#ea3345'
+    backgroundColor: '#ea3345', 
   }, 
 })
 
---
+++ tabbarAnimation/components/Tabbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, SafeAreaView} from 'react-native';
+
+const Tabbar = () => {
+  return (
+    <>
+        
+    </>
+  );
+};
+
+export default Tabbar;
